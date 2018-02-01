84f27a697282308cc75c9a6171e6338710dc03fd
adlotz <a.lotz@hotmail.fr>
2018-02-01 21:07:08 +0100

    final

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ApplicationRecord
 	belongs_to :user
-	has_many :comments
+	has_many :comments, dependent: :destroy
 
 	validates :url, presence: true
 end
--- db/migrate/20180201175214_create_comments.rb
+++ db/migrate/20180201175214_create_comments.rb
@@ -4,7 +4,6 @@ class CreateComments < ActiveRecord::Migration[5.1]
       t.text :commenter
       t.references :user, foreign_key: true
       t.references :pin, foreign_key: true
-
       t.timestamps
     end
   end
